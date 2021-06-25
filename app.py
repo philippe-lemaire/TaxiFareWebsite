8ebe7ca674e52a095399c3673886ce50e2139ed3
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-06-25 13:41:29 +0200

    working locally

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import datetime
+import requests
 
 """
 # TaxiFareModel front
@@ -16,37 +18,60 @@ Either as with the title by just creating a string (or an f-string). Or as with 
 ## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
 
 1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
+
+"""
+date = st.date_input(
+    label="Please select a date",
+    value=datetime.date.today(),
+    min_value=None,
+    max_value=None,
+    key=None,
+    help=None,
+)
+time = st.time_input(label="Please select a time", value=datetime.time())
+
+pickup_longitude = st.number_input(label="pickup longitude")
+pickup_latitude = st.number_input(label="pickup latitude")
+dropoff_longitude = st.number_input(label="dropoff longitude")
+dropoff_latitude = st.number_input(label="dropoff latitude")
+passenger_count = st.slider(
+    label="Passengers, it really does not matter",
+    min_value=1,
+    max_value=5,
+    value=1,
+    step=None,
+    format=None,
+    key=None,
+    help=None,
+)
+
 """
 
 """
 ## Once we have these, let's call our API in order to retrieve a prediction
-
+"""
 See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
 
 🤔 How could we call our API ? Off course... The `requests` package 💡
 """
 
-url = "https://taxifare.lewagon.ai/predict"
+url = "https://phil-benkklhp2a-ew.a.run.app/predict"
 
-if url == "https://taxifare.lewagon.ai/predict":
+params = {
+    "key": "whatever",
+    "pickup_datetime": "2001-12-26 12:12:00",
+    "pickup_longitude": pickup_longitude,
+    "pickup_latitude": pickup_latitude,
+    "dropoff_longitude": dropoff_longitude,
+    "dropoff_latitude": dropoff_latitude,
+    "passenger_count": passenger_count,
+}
 
-    st.markdown(
-        "Maybe you want to use your own API for the prediction, not the one provided by Le Wagon..."
-    )
+response = requests.get(url, params)
 
-"""
+pred = response.json()["prediction"]
+pred = round(pred, 2)
 
-2. Let's build a dictionary containing the parameters for our API...
+## Here is the result
 
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-"""
+f" Your fare should be about {pred} dollar"
